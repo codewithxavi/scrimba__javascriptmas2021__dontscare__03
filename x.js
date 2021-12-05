1e0f0ac7baba78f840bcc6cd4c5b8bf9e06102eb
xavicoder <70905472+xavi-coder@users.noreply.github.com>
2021-12-05 14:52:06 +0100

    Add files via upload

---
+++ x.js
@@ -0,0 +1,23 @@
+const greeting = document.getElementById("greeting")
+const btn = document.getElementById("btn")
+
+btn.addEventListener("click", fix)
+
+function fix() {
+  // Task:
+  // - Write a function to fix the UI problems with this Christmas message (make it Christmassy!)
+  // - Run the function when the Fix button is clicked.
+  greeting.textContent = "🎅🏻 Christmassy! 👑🎁👑🎁👑🎁"
+}
+
+
+//Stretch goals:
+// - Add an extra theme, and the option to switch between them.
+// - Change the message and theme to a New Year’s one automatically on December 31st.
+
+
+
+
+
+
+
